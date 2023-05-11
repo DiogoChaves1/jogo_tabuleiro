2104abb0ba417d260f0ac4ba94ac20bc2597eceb
DiogoChaves1 <118543063+DiogoChaves1@users.noreply.github.com>
2023-05-11 15:21:02 -0300

    Update and rename JogadorSortudo.java to JogadorAzarado.java

---
+++ JogadorAzarado.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+public class JogadorAzarado extends Jogador{
+
+    public JogadorAzarado(String cor, int jogadas ){
+        super(cor, jogadas);
+    }
+
+    public int jogarDados(){
+        Random gerador = new Random();
+        int dado1, dado2;
+
+        do{ 
+            dado1 = gerador.nextInt(6) + 1;
+            dado2 = gerador.nextInt(6) + 1;
+        } while( dado1 + dado2 > 6 );
+        
+        return dado1 + dado2;
+    }
+    
+}
--- JogadorSortudo.java
+++
@@ -1,4 +0,0 @@
-public class JogadorSortudo extends Jogador{
-    
-    
-}
