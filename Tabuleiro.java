0e02de191b0cc1df9f84437ff7858b5784defd0f
yuriolv <yuri_2505@icloud.com>
2023-05-15 08:37:54 -0300

    Adicionando as casas mágica e do azar

--- Tabuleiro.java
+++ Tabuleiro.java
@@ -18,27 +18,55 @@ public class Tabuleiro {
     }
 
     public void mostrarPosição(){
-        for(Jogador j: jogadores){
-        System.out.println(j.toString());
+        for(Jogador jogador: jogadores){
+            System.out.println(jogador.toString());
         }
     }
 
-    public void checkCasasDaSorte(Jogador j) {
-        if( j instanceof JogadorNormal || j instanceof JogadorSortudo) {
+    public void checkCasasDaSorte(Jogador jogador) {
+        if( jogador instanceof JogadorNormal || jogador instanceof JogadorSortudo) {
             System.out.println("Voce esta em uma casa da sorte. +3 casas");
-            j.posição += 3;
-            System.out.printf("Posicao atual: %d\n", j.posição);
+            jogador.posição += 3;
+            System.out.printf("Posicao atual: %d\n", jogador.posição);
         }
 
-        System.out.printf("Posicao atual: %d\n", j.posição);
+        System.out.printf("Posicao atual: %d\n", jogador.posição);
     }
 
-    public void casaDeVoltarAoInicio(String cor) {
-        for(Jogador j: jogadores) {
-            if(cor == j.cor) {
-                j.posição = 0;
+    public void checkCasaDeTroca(String cor) {
+        for(Jogador jogador: jogadores) {
+            if(cor == jogador.cor) {
+                jogador.posição = 0;
             }
         }
+    }
+
+    public boolean checkCasaDoAzar(Jogador jogador){
+        if(jogador.posição == 10 || jogador.posição == 25 || jogador.posição == 38){
+            return true;
+        }
+
+        return false;
+    }
+
+    public void checkCasaMagica(Jogador jogador){
+        int menorPosicao = jogador.posição;
+        int indexMenorPosicao = 0;
+
+        for (Jogador jogadorTroca : jogadores) {
+            if(jogadorTroca.posição < menorPosicao){
+                menorPosicao = jogadorTroca.posição;
+                indexMenorPosicao = jogadores.indexOf(jogadorTroca);
+            }
+        }
+        if(menorPosicao == jogador.posição) {  
+            System.out.println("O jogador " + jogador.cor  + "ja esta na ultima posicao... :´( )");
+            
+            return;
+        }
+        jogadores.get(indexMenorPosicao).posição = jogador.posição;
+        jogador.posição = menorPosicao;
+
     }
     
     public int getRodada(){
